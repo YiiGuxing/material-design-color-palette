af7c3ab683615f2621d6ed11a44d0f36c770cb57
Yii.Guxing <yii.guxing@gmail.com>
2018-06-25 23:57:43 +0800

    Update material palette form.

--- src/main/java/cn/yiiguxing/plugin/md/colorswatches/MaterialPaletteForm.java
+++ src/main/java/cn/yiiguxing/plugin/md/colorswatches/MaterialPaletteForm.java
@@ -1,6 +1,24 @@
 package cn.yiiguxing.plugin.md.colorswatches;
 
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBScrollPane;
+
 import javax.swing.*;
 
 public class MaterialPaletteForm {
+    public JPanel rootPanel;
+    public JPanel headerPanel;
+    public JBScrollPane leftScrollPane;
+    public JPanel namesPanel;
+    public JBScrollPane contentScrollPane;
+    public JPanel contentPanel;
+    public JPanel primaryPreviewPanel;
+    public JPanel lightPreviewPanel;
+    public JPanel darkPreviewPanel;
+    public JBLabel primaryColorLabel;
+    public JBLabel lightColorLabel;
+    public JBLabel darkColorLabel;
+    public JLabel primaryPreviewTitle;
+    public JLabel lightPreviewTitle;
+    public JLabel darkPreviewTitle;
 }
